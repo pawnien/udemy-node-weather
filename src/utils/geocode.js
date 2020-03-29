973ce6f9ee0c09a78fbbc25afd8fff2cd6356cff
paweln <paweln@sparkbit.pl>
2020-03-29 20:26:14 +0200

    Initial commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,25 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoicGx1dG8xOTg5IiwiYSI6ImNrM3l4M252ZDBxZmYzbHM4ZW10aXF5MmoifQ.llIfugYoeUzsGrRo2vmRmg&limit=1";
+
+  request({ url, json: true }, (error, { body }) => {
+    // console.log("body", body.features.length);
+    if (error) {
+      callback("Unable to connect to location services!", undefined);
+    } else if (body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: body.features[0].center[1],
+        longitude: body.features[0].center[0],
+        location: body.features[0].place_name
+      });
+    }
+  });
+};
+
+module.exports = geocode;
